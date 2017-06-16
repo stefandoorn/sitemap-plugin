fb7ae746421bbad90913dcf4d8462c0b940f5dc7
Stefan Doorn <stefan@efectos.nl>
2017-06-16 11:35:27 +0200

    Adjust specs to include adjusted TwigAdapter

--- spec/SitemapPlugin/Renderer/TwigAdapterSpec.php
+++ spec/SitemapPlugin/Renderer/TwigAdapterSpec.php
@@ -16,7 +16,7 @@ final class TwigAdapterSpec extends ObjectBehavior
 {
     function let(EngineInterface $twig)
     {
-        $this->beConstructedWith($twig, '@SyliusCore/Sitemap/url_set.xml.twig');
+        $this->beConstructedWith($twig, '@SyliusCore/Sitemap/url_set.xml.twig', false);
     }
 
     function it_is_initializable()
@@ -32,7 +32,7 @@ final class TwigAdapterSpec extends ObjectBehavior
     function it_renders_sitemap($twig, SitemapInterface $sitemap, SitemapUrlInterface $productUrl)
     {
         $sitemap->getUrls()->willReturn([$productUrl]);
-        $twig->render('@SyliusCore/Sitemap/url_set.xml.twig', ['url_set' => [$productUrl]])->shouldBeCalled();
+        $twig->render('@SyliusCore/Sitemap/url_set.xml.twig', ['url_set' => [$productUrl], 'absolute_url' => false])->shouldBeCalled();
 
         $this->render($sitemap);
     }
