b46c0534af7319d56b4c4a1375e76962fedf581f
Stefan Doorn <stefan@efectos.nl>
2017-09-09 16:57:22 +0200

    Adjust return type in test app

--- tests/Application/app/AppKernel.php
+++ tests/Application/app/AppKernel.php
@@ -9,7 +9,7 @@ final class AppKernel extends Kernel
     /**
      * {@inheritdoc}
      */
-    public function registerBundles()
+    public function registerBundles(): array
     {
         return array_merge(parent::registerBundles(), [
             new \Sylius\Bundle\AdminBundle\SyliusAdminBundle(),
