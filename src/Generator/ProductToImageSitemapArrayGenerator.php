e92f75183762b20e9ea0e5290f56cf8beae15faa
Stefan Doorn <stefan@efectos.nl>
2019-01-22 23:35:17 +0100

    Add image when path is set, not only when SplInfo is present

--- src/Generator/ProductToImageSitemapArrayGenerator.php
+++ src/Generator/ProductToImageSitemapArrayGenerator.php
@@ -36,7 +36,7 @@ final class ProductToImageSitemapArrayGenerator
 
         /** @var ProductImageInterface $image */
         foreach ($product->getImages() as $image) {
-            if (!$image->hasFile() || !$image->getPath()) {
+            if (!$image->getPath()) {
                 continue;
             }
 
