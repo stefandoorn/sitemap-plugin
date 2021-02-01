2c108ae991a0d92ad5ecd70e5910784a8215494b
Stefan Doorn <stefan@efectos.nl>
2021-02-01 23:55:50 +0100

    No teardown

--- tests/Controller/TearDownTrait.php
+++ tests/Controller/TearDownTrait.php
@@ -12,6 +12,8 @@ trait TearDownTrait
 {
     public function tearDown(): void
     {
+        return;
+        
         if (null !== $this->client && null !== $this->client->getContainer()) {
             $dir = $this->client->getContainer()->getParameter('sylius.sitemap.path');
 
