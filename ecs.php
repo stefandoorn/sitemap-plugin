a5e3736f1b59badd1303e74b9e12195aea7a6e95
Stefan Doorn <stefan@efectos.nl>
2021-12-12 13:34:12 +0100

    Fix ECS

--- ecs.php
+++ ecs.php
@@ -1,14 +1,21 @@
 <?php
 
+use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set('exclude_files', ['tests/Application/*']);
+    $parameters->set(Option::SKIP, [
+        VisibilityRequiredFixer::class => ['*Spec.php'],
+        'tests/Application/*',
+    ]);
 
     $services = $containerConfigurator->services();
-    $services->set(NativeFunctionInvocationFixer::class);
+    $services->set(
+        NativeFunctionInvocationFixer::class
+    )->call('configure', [['include' => ['@all'], 'scope' => 'all', 'strict' => \true]]);
 };
