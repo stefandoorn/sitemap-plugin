a34a9bb17476cc32e2cb2475247d0be8625279fb
Stefan Doorn <stefan@efectos.nl>
2021-12-12 14:25:05 +0100

    Fix

--- src/Routing/SitemapLoader.php
+++ src/Routing/SitemapLoader.php
@@ -8,7 +8,6 @@ use SitemapPlugin\Builder\SitemapBuilderInterface;
 use SitemapPlugin\Exception\RouteExistsException;
 use Symfony\Bundle\FrameworkBundle\Routing\RouteLoaderInterface;
 use Symfony\Component\Config\Loader\Loader;
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
