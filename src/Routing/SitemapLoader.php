8e39e7554e7f582498d95250e4d799f858359227
Stefan Doorn <stefan@efectos.nl>
2021-12-12 12:19:09 +0100

    Adjust type declaration

--- src/Routing/SitemapLoader.php
+++ src/Routing/SitemapLoader.php
@@ -26,7 +26,7 @@ final class SitemapLoader extends Loader implements ContainerAwareInterface
         $this->sitemapBuilder = $sitemapBuilder;
     }
 
-    public function load($resource, string $type = null): RouteCollection
+    public function load($resource, string $type = null)
     {
         $routes = new RouteCollection();
 
