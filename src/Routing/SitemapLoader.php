dc9c5e08352aaa3f07e29bb7c085e824017d800e
Stefan Doorn <stefan@efectos.nl>
2023-03-23 10:05:37 +0100

    Revert "Coding standard fixes"
    This reverts commit af431a03cf2e937b01b4fffb4f1b05fbfc282f69.

--- src/Routing/SitemapLoader.php
+++ src/Routing/SitemapLoader.php
@@ -51,8 +51,8 @@ final class SitemapLoader extends Loader implements RouteLoaderInterface
                     [],
                     '',
                     [],
-                    ['GET'],
-                ),
+                    ['GET']
+                )
             );
         }
 
