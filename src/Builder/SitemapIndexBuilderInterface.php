1310efb3b9db3b6dfa352f48cfae1c627ccebcfb
Ajay Singh <ajay@planet-ride.com>
2023-05-01 17:04:15 +0530

    Update SitemapIndexBuilderInterface.php
    SitemapIndexBuilder interface accept new param as channel

--- src/Builder/SitemapIndexBuilderInterface.php
+++ src/Builder/SitemapIndexBuilderInterface.php
@@ -6,10 +6,11 @@ namespace SitemapPlugin\Builder;
 
 use SitemapPlugin\Model\SitemapInterface;
 use SitemapPlugin\Provider\IndexUrlProviderInterface;
-
+use Sylius\Component\Core\Model\ChannelInterface;
 interface SitemapIndexBuilderInterface extends BuilderInterface
 {
     public function addIndexProvider(IndexUrlProviderInterface $indexProvider): void;
 
-    public function build(): SitemapInterface;
+    public function build(ChannelInterface $channel): SitemapInterface;
 }
+
