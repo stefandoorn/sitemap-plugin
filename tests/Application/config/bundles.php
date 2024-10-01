621034d71ba36595e1eac9c8f68fd1a4d006e51c
Stefan Doorn <stefan@efectos.nl>
2024-10-01 14:53:17 +0200

    Load Sylius calendar plugin

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -62,4 +62,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
  ] + $previousSyliusVersions;
